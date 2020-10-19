60cdd91578337f96c28d0c975d504ddb396ae303
M Estey <smileyegg@gmail.com>
2020-10-19 10:20:58 -0600

    Final Update

--- static/js/logic.js
+++ static/js/logic.js
@@ -21,17 +21,22 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_we
   function markerSize(mags){
     return mags*35000;
   }
-  // var color="";
-  // if (features[i].properties.mag>5) {
-  //   color="red";
-  // }
 
-  function color(d){
-    return d>5.5?'red':
-           d>4.5?'yellow':
-           d>3.1?'green':
-           d>2.4?'blue':
-                '#00FFFF';
+function color(magnitude){
+  switch (true) {
+      case magnitude>=5:
+          return "red";
+      case magnitude>=4:
+          return "orange";
+      case magnitude>=3:
+          return "yellow";
+      case magnitude>=2:
+          return "green"; 
+      case magnitude>=0:
+          return "green";     
+      default:
+          return "green";
+  }
 }
 
 // Perform a GET request to the query URL
@@ -42,28 +47,29 @@ d3.json(queryUrl, function(data) {
   for( var i=0; i <data.features.length; i++){
 // setting the markerSize
     L.circle(data.features[i].geometry.coordinates.reverse().slice(1), {
-            fillOpacity:0.5,
-            color: 'blue',
+            fillOpacity:0.75,
+            color: 'black',
             fillColor:color(data.features[i].properties.mag),
             radius:markerSize(data.features[i].properties.mag)
   // Bind popup- so when you click on the point it gives you data
-  
+
         }).bindPopup("<h1>" + data.features[i].properties.place + "</h1><hr><h3>Magnitude: "+data.features[i].properties.mag + "</h3> </h3>Time:" + new Date(data.features[i].properties.time)+ "</h3>")
         .addTo(myMap);
   }
-
+// Add a Legend
   var legend=L.control({position:'bottomright'});
 
-  legend.onAdd=function(map){
+  legend.onAdd=function(myMap){
   
       var div=L.DomUtil.create('div', 'info legend'),
-          grades=[0,2.5,4.5,5.6],
+          mags=[0,2,3,4,5],
           labels=[];
   
-          for (var i=0; i<grades.length; i++){
+          for (var i=0; i< mags.length; i++){
+            console.log(mags[i] + labels[i]);
               div.innerHTML +=
-                  '<i style="background:' + color(grades[i]+1)+ '"></i>'+
-                  grades[i]+(grades[i+1]?'&ndash;' +grades[i+1] + '<br>' : '+'); 
+                  '<i style="background:' + color(mags[i]+1)+ '"></i>'+
+                  mags[i]+(mags[i+1]?'&ndash;' +mags[i+1] + '<br>' : '+'); 
           }
   
           return div;
